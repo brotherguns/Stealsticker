f09b6ddf42a192285a7e6105320e075a9763b795
brotherguns <braydenr209@gmail.com>
2026-04-01 00:16:17 -0500

    Add files via upload

--- src/StealSticker/ui/sheets/AddToServerActionSheet.tsx
+++ src/StealSticker/ui/sheets/AddToServerActionSheet.tsx
@@ -12,44 +12,56 @@ import {
 } from "../../modules";
 import { getStickerUrl } from "../../lib/utils/getStickerUrl";
 
-const { FormDivider, FormIcon } = Forms;
+var FormDivider = Forms?.FormDivider;
+var FormIcon = Forms?.FormIcon;
 
 export function showAddToServerActionSheet(sticker: StickerNode) {
+    if (!LazyActionSheet?.openLazy) return;
     LazyActionSheet.openLazy(
-        Promise.resolve({ default: () => <AddToServer sticker={sticker} /> }),
+        Promise.resolve({ default: function() { return <AddToServer sticker={sticker} />; } }),
         "AddToServerStickerActionSheet"
     );
 }
 
 function AddToServer({ sticker }: { sticker: StickerNode }) {
-    const guilds = (Object.values(GuildStore.getGuilds()) as any[])
-        .filter(g => PermissionsStore.can(constants.Permissions.MANAGE_GUILD_EXPRESSIONS, g))
-        .sort((a, b) => a.name?.localeCompare?.(b.name));
+    var guilds = (Object.values(GuildStore?.getGuilds?.() ?? {}) as any[])
+        .filter(function(g: any) {
+            return PermissionsStore?.can?.(constants?.Permissions?.MANAGE_GUILD_EXPRESSIONS, g);
+        })
+        .sort(function(a: any, b: any) { return a.name?.localeCompare?.(b.name); });
 
-    const previewUrl = getStickerUrl(sticker, 64);
+    var previewUrl = getStickerUrl(sticker, 64);
 
     return (
         <ActionSheet scrollable>
             <ErrorBoundary>
-                <ActionSheetTitleHeader
-                    title={`Stealing ${sticker.name}`}
-                    leading={
-                        previewUrl
-                            ? <FormIcon style={{ marginRight: 12, opacity: 1 }} source={{ uri: previewUrl }} disableColor />
-                            : undefined
-                    }
-                    trailing={<ActionSheetCloseButton onPress={() => LazyActionSheet.hideActionSheet()} />}
-                />
-                <BottomSheetFlatList
-                    style={{ flex: 1 }}
-                    contentContainerStyle={{ paddingBottom: 24 }}
-                    data={guilds}
-                    renderItem={({ item }: { item: any }) => (
-                        <AddToServerRow guild={item} sticker={sticker} />
-                    )}
-                    ItemSeparatorComponent={FormDivider}
-                    keyExtractor={(x: any) => x.id}
-                />
+                {ActionSheetTitleHeader ? (
+                    <ActionSheetTitleHeader
+                        title={"Stealing " + sticker.name}
+                        leading={
+                            previewUrl && FormIcon
+                                ? <FormIcon style={{ marginRight: 12, opacity: 1 }} source={{ uri: previewUrl }} disableColor />
+                                : undefined
+                        }
+                        trailing={
+                            ActionSheetCloseButton
+                                ? <ActionSheetCloseButton onPress={function() { LazyActionSheet?.hideActionSheet?.(); }} />
+                                : undefined
+                        }
+                    />
+                ) : null}
+                {BottomSheetFlatList ? (
+                    <BottomSheetFlatList
+                        style={{ flex: 1 }}
+                        contentContainerStyle={{ paddingBottom: 24 }}
+                        data={guilds}
+                        renderItem={function({ item }: { item: any }) {
+                            return <AddToServerRow guild={item} sticker={sticker} />;
+                        }}
+                        ItemSeparatorComponent={FormDivider}
+                        keyExtractor={function(x: any) { return x.id; }}
+                    />
+                ) : null}
             </ErrorBoundary>
         </ActionSheet>
     );
